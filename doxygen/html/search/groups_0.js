0e82f17e14098357c4c4379ef29e2970bed92b2c
gmarull <gerard@teslabs.com>
2022-08-16 16:19:50 +0000

    Deploying to gh-pages from @ teslabs/spinner@5591731a547a9d5ee50636360122e53625cacb4b 🚀

---
+++ doxygen/html/search/groups_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['current_20sampling_20api_101',['Current Sampling API',['../group__spinner__drivers__currsmp.html',1,'']]],
+  ['control_20apis_102',['Control APIs',['../group__spinner__lib__control.html',1,'']]],
+  ['current_20loop_20api_103',['Current Loop API',['../group__spinner__lib__control__cloop.html',1,'']]]
+];
